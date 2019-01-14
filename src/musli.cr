0c12925363bf2e33253f7534c5318bdbf6b2021c
Niels Nielsen <slein.nroh@gmail.com>
2019-01-14 15:53:09 +0100

    Add slang template for chat

--- src/musli.cr
+++ src/musli.cr
@@ -1,6 +1,25 @@
-# TODO: Write documentation for `Musli`
-module Musli
-  VERSION = "0.1.0"
+require "kemal"
 
-  # TODO: Put your code here
+require "slang"
+require "kilt"
+require "kilt/slang"
+
+SOCKETS = [] of HTTP::WebSocket
+
+get "/" do
+  render "views/index.slang"
 end
+
+ws "/chat" do |socket|
+  SOCKETS << socket
+
+  socket.on_message do |message|
+    SOCKETS.each { |socket| socket.send message }
+  end
+
+  socket.on_close do
+    SOCKETS.delete socket
+  end
+end
+
+Kemal.run
